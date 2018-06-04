6e2e194b6e22676f4d8b25a51f3cd124c1a6bf80
agpiermarini <piermarini.andrew@gmail.com>
2018-06-04 13:50:33 -0600

    Troubleshoot asset pipeline issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # config.public_file_server.enabled = true
-  config.public_file_server.enabled = ENV['SERVE_STATIC_FILES']
 end
