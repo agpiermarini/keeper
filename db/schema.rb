de731e57dbfd63b54fe56576aff62a56e9b3afb5
agpiermarini <piermarini.andrew@gmail.com>
2018-09-28 18:49:50 -0600

    Add preliminary logic for PersonalityProfileText#Show. Add PersonalityProfileGenerator#generate_from_text. Add source column to PersonalityProfiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_03_011442) do
+ActiveRecord::Schema.define(version: 2018_09_28_234810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_06_03_011442) do
     t.string "oldest_tweet_analyzed_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
   create_table "saved_profiles", force: :cascade do |t|
