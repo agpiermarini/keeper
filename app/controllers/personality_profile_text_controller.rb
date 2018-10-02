7c2ba66991fac971b6ac4afdba6e1e1391e372dc
agpiermarini <piermarini.andrew@gmail.com>
2018-10-01 18:45:26 -0600

    Add strong params

--- app/controllers/personality_profile_text_controller.rb
+++ app/controllers/personality_profile_text_controller.rb
@@ -2,7 +2,12 @@ class PersonalityProfileTextController < ApplicationController
   def new; end
 
   def show
-    PersonalityProfileGenerator.new(nil, params).from_text!
+    PersonalityProfileGenerator.new(nil, profile_params).from_text!
     @twitter_profile = TwitterProfileSearch.new(current_user.username).profile
   end
+
+  private
+    def profile_params
+      params.permit(:name, :source, :text)
+    end
 end
